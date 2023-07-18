54f7d69f67aa441381143dd948b23ab52240877d
syngrxm <smcubez48@gmail.com>
2023-07-18 14:08:21 +0900

    이새기때려죽여도에러존나남개빡침

--- routes/sendImg.js
+++ routes/sendImg.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const multer = require("multer");
 const cors = require("cors");
+const database = require('../firebase')
 // cors: Cross-Site Http Request를 가능하게 하는 표준 규칙
 // 웹브라우저가 XMLHttpRequest나 Fetch API로 외부서버의 데이터에
 // 접근하려는 경우, 보안 상의 이유로 브라우저는 CORS를 제한
@@ -25,14 +26,17 @@ const upload = multer({ storage: imgStorage }); // 파일 업로드 객체
 router.post("/sendImg", upload.single("file"), (req, res) => {
   const { imgName, imgData } = req.body;
   const dcdImg = Buffer.from(imgData, "base64"); // 디코딩
+  bucket.upload(dcdImg));
 
+
+/*
   fs.writeFile(imgName, dcdImg, (err) => {
     // fs.writeFile: 이미지 파일 ㅅ버에 저장
     if (err) {
       console.error(err);
       res.status(500).json({ error: "이미지를 저장하는 데에 실패했습니다. " });
     } else res.json({ message: "이미지 저장이 성공적으로 완료되었습니다!" });
-  });
+  }); */
 });
 
 module.exports = router;
