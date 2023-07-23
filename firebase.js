b2e99d6737308f03b27aa332087ee778db63bd56
syngrxm <smcubez48@gmail.com>
2023-07-24 00:56:37 +0900

    -

--- firebase.js
+++ firebase.js
@@ -13,19 +13,8 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const admin = require("firebase-admin");
-const serviceAccount = {
-  "type": process.env.type,
-  "project_id": process.env.project_id,
-  "private_key_id": process.env.private_key_id,
-  "private_key": process.env.private_key,
-  "client_email": process.env.lient_email,
-  "client_id": process.env.client_id,
-  "auth_uri": process.env.auth_uri,
-  "token_uri": process.env.token_uri,
-  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-  "client_x509_cert_url": process.env.client_x509_cert_url,
-  "universe_domain": process.env.universe_domain
-};
+
+const serviceAccount = require("./firstimg-f2b2d-firebase-adminsdk-far95-deecf8176f.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
